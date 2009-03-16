62b9368c183f0996ee550d01c5652288db16dbb9
John Stowers <john.stowers@gmail.com>
2009-03-16 16:41:54 +1300

    add more useful definitions to config

--- john/config.h
+++ john/config.h
@@ -1,6 +1,11 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
+#include <dc1394/dc1394.h>
+
 #define MY_CAMERA_GUID 0xb09d0100818d56LL
+#define MY_CAMERA_W 640
+#define MY_CAMERA_H 480
+#define MY_CAMERA_CODING DC1394_COLOR_CODING_RGB8
 
 #endif
